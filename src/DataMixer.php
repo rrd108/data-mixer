514492437f16f24dfdab42e9b16ee378c94969fe
rrd108 <rrd@1108.cc>
2017-12-01 19:31:19 +0100

    whole class refactored

--- src/DataMixer.php
+++ src/DataMixer.php
@@ -18,10 +18,10 @@ class DataMixer
     public function __construct(array $options)
     {
         $this->mysqli = new mysqli(
-            $options['host'],
+            $options['dbHost'],
             $options['dbUser'],
             $options['dbPassword'],
-            $options['database']
+            $options['dbName']
         );
 
         if ($this->mysqli->connect_errno) {
@@ -29,49 +29,80 @@ class DataMixer
         }
     }
 
-    public function mix()
+    /**
+     * Get mixed rows
+     *
+     * @param string $table
+     * @param array  $fields
+     *  simple array like ['first_name', 'last_name']
+     *  or dependent array like ['first_name'=> 'sex', 'last_name']
+     * @return mixed
+     */
+    public function getMixed(string $table, array $fields)
     {
-        $updated = 0;
-        $values = $ids = [];
-        if ($result = $this->mysqli->query('SELECT * FROM ' . $table)) {
-            while ($row = $result->fetch_assoc()) {
-                $ids[] = $row['id'];
-                $values[$row['id']] = $row;
-            }
-
-            $result->close();
-        }
+        $rows = $this->getRows($table);
+        $ids = array_keys($rows);
 
         foreach ($ids as $id) {
-            foreach ($fields as $field) {
+            foreach ($fields as $key => $field) {
+                if (is_numeric($key)) {
+                    $row = $rows[array_rand($rows)];
+                    if ($row[$field]) {
+                        $mixed[$table][$id][$field] = $row[$field];
+                    }
+                }
                 //if $field is an array, we should random select a row
                 //where the dependent field has the same value than the current
-                if (is_array($field)) {
-                    $filtered_values = [];
-                    $key = key($field);
-                    foreach ($values as $val) {
-                        if ($val[$field[$key]] == $values[$id][$field[$key]]) {
-                            $filtered_values[] = $val;
+                if (!is_numeric($key)) {
+                    $filtered_rows = [];
+                    //collect rows with same key
+                    foreach ($rows as $row) {
+                        if ($row[$field] == $rows[$id][$field]) {
+                            $filtered_rows[] = $row;
                         }
                     }
-                    $row = $filtered_values[array_rand($filtered_values)];
+                    $row = $filtered_rows[array_rand($filtered_rows)];
                     if ($row[$key]) {
-                        $this->mysqli->query('UPDATE ' . $table
-                            . ' set ' . $key . ' = "' . $row[$key] . '" WHERE id = ' . $id);
-                        $updated++;
-                    }
-                } else {
-                    $row = $values[array_rand($values)];
-                    if ($row[$field]) {
-                        $this->mysqli->query('UPDATE ' . $table
-                            . ' set ' . $field . ' = "' . $row[$field]
-                            . '" WHERE id = ' . $id);
-                        $updated++;
+                        $mixed[$table][$id][$key] = $row[$key];
                     }
                 }
             }
         }
+        return $mixed;
+    }
 
+    public function updateRows(array $mixed)
+    {
+        //TODO use prepared statements
+        foreach ($mixed as $table => $row) {
+            foreach ($row as $id => $data) {
+                $sql = 'UPDATE ' . $table . ' SET ';
+                foreach ($data as $key => $value) {
+                    $sql .= $key . ' = "' . $value . '", ';
+                }
+                $sql = rtrim($sql, ', ');
+                $sql .= ' WHERE id = ' . $id .';';
+                //$this->mysqli->query($sql);
+                print $sql . "\n";
+            }
+        }
+    }
+
+    public function getRows(string $table)
+    {
+        $result = $this->mysqli->query('SELECT * FROM ' . $table);
+        $values = [];
+
+        while ($row = $result->fetch_assoc()) {
+            $values[$row['id']] = $row;
+        }
+        $result->close();
+
+        return $values;
+    }
+
+    protected function close()
+    {
         $this->mysqli->close();
     }
 }
--- tests/DataMixerTest.php
+++ tests/DataMixerTest.php
@@ -2,21 +2,62 @@
 namespace DataMixer\Tests;
 
 use DataMixer\DataMixer;
-use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class DataMixerTest extends TestCase
 {
-    public function testConstruct()
+    public function setUp()
     {
-        $this->expectException(InvalidArgumentException::class);
-        new DataMixer(
+        parent::setUp();
+        $this->dataMixer = new DataMixer(
             [
-                'host' => 'localhost',
-                'dbUser' => 'user',
-                'dbPassword' => 'supersecret',
-                'database' => 'test'
+                'dbHost' => 'localhost',
+                'dbUser' => 'root',
+                'dbPassword' => '123',
+                'dbName' => 'test'
             ]
         );
+        //TODO insert fixtures
+    }
+
+    public function testGetRows()
+    {
+        $actual = $this->dataMixer->getRows('users');
+        $this->assertEquals(
+            ['id' => 1, 'first_name' => 'Anna', 'last_name' => 'Algol', 'sex' => 'F'],
+            $actual[1]
+        );
+    }
+
+    public function testSimpleMix()
+    {
+        //TODO mock array_rand replace with array reverse
+        $actual = $this->dataMixer->getMixed('users', ['first_name', 'last_name']);
+        $this->assertNotEquals(
+            ['id' => 1, 'first_name' => 'Anna', 'last_name' => 'Algol', 'sex' => 'F'],
+            $actual['users'][1]
+        );
+    }
+
+    public function testDependentMix()
+    {
+        //TODO mock array_rand replace with array reverse
+        $actual = $this->dataMixer->getMixed('users', ['first_name'=> 'sex', 'last_name']);
+        $this->assertNotEquals(
+            ['id' => 1, 'first_name' => 'Anna', 'last_name' => 'Algol', 'sex' => 'F'],
+            $actual['users'][1]
+        );
+        $this->assertEquals('Bella', $actual['users'][2]['first_name']);
+        $this->assertEquals('Daniel', $actual['users'][4]['first_name']);
+    }
+
+    public function testUpdateRows()
+    {
+        $mixed = $this->dataMixer->getMixed('users', ['first_name'=> 'sex', 'last_name']);
+        $actual = $this->dataMixer->updateRows($mixed);
+
+        $mixed = $this->dataMixer->getMixed('users', ['first_name', 'last_name']);
+        $actual = $this->dataMixer->updateRows($mixed);
+
     }
 }
